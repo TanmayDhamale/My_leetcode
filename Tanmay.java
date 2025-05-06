bd6ff7e92697f2f9762c592575b40004c25460c2
Tanmay Dhamale <tanmaydhamale@gmail.com>
2025-05-06 11:49:18 +0530

    Refactor Tanmay implementation to improve user interaction and code clarity

--- Tanmay.java
+++ Tanmay.java
@@ -1,20 +1,39 @@
+// import java.util.Scanner;
+
+// public class Tanmay {
+//     public static void main(String[] args)
+//     {
+//         System.out.println ("Enter your name ");
+//         Scanner sc = new Scanner(System.in);
+//         String name = sc.nextLine();
+
+//         // System.out.println("Welcome"+" "+name);
+
+//         System.out.println("Relation with Tanmay ?");
+//         String relation = sc.nextLine();
+
+//         // System.out.println("Your relation with Tanmay is"+"  "+relation);
+//         System.out.println("welcome"   + name + " , Tanmay's"   + relation);
+//     }
+    
+    
+// }
 import java.util.Scanner;
 
 public class Tanmay {
-    public static void main(String[] args)
-    {
-        System.out.println ("Enter your name ");
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome"+" "+name);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine().trim(); // .trim() removes extra spaces
 
-        System.out.println("Relation with Tanmay ?");
-        String relation = sc.nextLine();
+        System.out.print("Your relation with Tanmay: ");
+        String relation = scanner.nextLine().trim();
 
-        System.out.println("Your relation with Tanmay is"+"  "+relation);
-        System.out.println("welcome" + name + ", Tanmay's" + relation);
+        System.out.println("\nWelcome " + name + "!");
+        System.out.println("Your relation with Tanmay is: " + relation);
+        System.out.println("Welcome " + name + ", Tanmay's " + relation + "!");
+
+        scanner.close(); // Always close Scanner
     }
-    
-    
 }
